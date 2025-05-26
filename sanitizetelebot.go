42c8a54c364ef373504b78310e81903286b054b8
Mecid <mecid@mecomediagroup.de>
2025-05-26 12:10:12 +0200

    Feature: Keep Reply to messages (#7)
    * Feature: Keep Reply to messages
    Fixes #3
    
    * Make Markdown Escape its own function

--- sanitizetelebot.go
+++ sanitizetelebot.go
@@ -82,16 +82,24 @@ func main() {
 		}
 
 		if sanitized {
+			// Create send options with reply if original was a reply
+			sendOpts := &telebot.SendOptions{ParseMode: telebot.ModeMarkdown}
+			if m.IsReply() {
+				sendOpts.ReplyTo = m.ReplyTo
+			}
+
 			if isPhotoURL && len(photoURLs) > 0 {
 				// Create album of photos with caption on first photo
 				album := make(telebot.Album, 0)
 				for i, photoPath := range photoURLs {
 					var photo *telebot.Photo
 					if i == 0 {
+						// Escape brackets in the URL to prevent Markdown parsing issues
+						escapedURL := escapeMarkdown(m.Text)
 						// Add caption to first photo
 						photo = &telebot.Photo{
 							File:    telebot.FromDisk(photoPath),
-							Caption: fmt.Sprintf("@%s said: [Original Link](%s)", username, m.Text),
+							Caption: fmt.Sprintf("@%s said: [Original Link](%s)", username, escapedURL),
 						}
 					} else {
 						photo = &telebot.Photo{File: telebot.FromDisk(photoPath)}
@@ -99,8 +107,8 @@ func main() {
 					album = append(album, photo)
 				}
 
-				// Send the album
-				_, err := b.SendAlbum(m.Chat, album, &telebot.SendOptions{ParseMode: telebot.ModeMarkdown})
+				// Send the album with reply
+				_, err := b.SendAlbum(m.Chat, album, sendOpts)
 				if err != nil {
 					log.Printf("Failed to send album: %v", err)
 				}
@@ -110,13 +118,25 @@ func main() {
 					os.Remove(photoPath)
 				}
 			} else {
+				var err error
+				// Escape any Markdown special characters in the sanitized URL
+				escapedMsg := escapeMarkdown(sanitizedMsg)
+
 				if m.FromGroup() && strings.Contains(m.Text, "anon") {
-					b.Send(m.Chat, strings.Replace(sanitizedMsg, "anon", "", 1))
+					_, err = b.Send(m.Chat, strings.Replace(escapedMsg, "anon", "", 1), sendOpts)
 				} else {
-					b.Send(m.Chat, "@"+username+" said: "+sanitizedMsg)
+					_, err = b.Send(m.Chat, "@"+username+" said: "+escapedMsg, sendOpts)
+				}
+				if err != nil {
+					log.Printf("Failed to send message: %v", err)
+					return
 				}
 			}
-			b.Delete(m)
+
+			// Only try to delete the original message if we successfully sent the new one
+			if err := b.Delete(m); err != nil {
+				log.Printf("Failed to delete original message: %v", err)
+			}
 		}
 	})
 
@@ -448,6 +468,15 @@ func ExpandUrl(shortURL string) (string, error) {
 		return "", fmt.Errorf("received non-200 status code")
 	}
 	return resp.Request.URL.String(), nil
+}
+
+func escapeMarkdown(text string) string {
+	text = strings.ReplaceAll(text, "[", "\\[")
+	text = strings.ReplaceAll(text, "]", "\\]")
+	text = strings.ReplaceAll(text, "_", "\\_")
+	text = strings.ReplaceAll(text, "*", "\\*")
+	text = strings.ReplaceAll(text, "`", "\\`")
+	return text
 }
 
 func downloadImage(imageURL string) (string, error) {
